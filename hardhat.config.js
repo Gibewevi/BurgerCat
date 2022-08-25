de852582b9ff36efc1925c89fa354df7a2cc94cb
gibewevi <gibewevi@skiff.com>
2022-08-25 19:40:07 +0200

    rinkeby update deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,34 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-etherscan");
+const { task } = require("hardhat/config");
+let secrets = require('./secret.json');
+require("dotenv").config();
+const { INFURA, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.16",
+  solidity: {
+    version: "0.8.16",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      }
+    }
+  },
   paths: {
     artifacts: './artifacts'
   },
+  defaultNetwork: "rinkeby",
   networks: {
-    hardhat: {
-      chainId: 1337
+    rinkeby : {
+      url: INFURA,
+      accounts: [`0x${PRIVATE_KEY}`]
     }
+  },
+  etherscan: {
+    apiKey:ETHERSCAN_API_KEY
   }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,27 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
+const { MerkleTree } = require('merkletreejs');
+const keccak256 = require('keccak256');
+const tokens = require('../tokens.json');
+
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  let tab = [];
+  tokens.map((token) => {
+  tab.push(token.address);
+  });
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log("test");
+  const leaves = tab.map((address) => keccak256(address));
+  const tree = new MerkleTree(leaves, keccak256, { sort: true});
+  const root = tree.getHexRoot();
+  const baseURI= "ipfs://QmV1T9XWiyb6Azk2Y7bE1PmtabHeESmWdKP5GJk7vFYbB6/";
 
-  await lock.deployed();
+  console.log("test");
+const Contract = await hre.ethers.getContractFactory("BurgerCatERC721A");
+const contract = await Contract.deploy(root, baseURI);
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+await contract.deployed();
+
+console.log("Contract deployed to:", contract.address + " - root"+root+" baseURI " + baseURI);
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
