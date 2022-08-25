e2b3448e5df1ad6036d46b819df46652065c916e
gibewevi <gibewevi@skiff.com>
2022-08-25 21:28:37 +0200

    rinkeby deployed success

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,17 +11,22 @@ async function main() {
   tab.push(token.address);
   });
 
-  console.log("test");
   const leaves = tab.map((address) => keccak256(address));
   const tree = new MerkleTree(leaves, keccak256, { sort: true});
   const root = tree.getHexRoot();
   const baseURI= "ipfs://QmV1T9XWiyb6Azk2Y7bE1PmtabHeESmWdKP5GJk7vFYbB6/";
 
-  console.log("test");
-const Contract = await hre.ethers.getContractFactory("BurgerCatERC721A");
-const contract = await Contract.deploy(root, baseURI);
+  const Contract = await hre.ethers.getContractFactory("BurgerCatERC721A");
+  const contract = await Contract.deploy(root, baseURI);
 
 await contract.deployed();
 
 console.log("Contract deployed to:", contract.address + " - root"+root+" baseURI " + baseURI);
 }
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
